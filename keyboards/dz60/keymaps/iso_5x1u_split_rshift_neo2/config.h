54582ad23ef1554a774071ba2eea2f5face6e74a
IantoKX <jan@stante-pe.de>
2024-09-29 15:32:12 +0200

    set up rescue repo for qmk

---
+++ keyboards/dz60/keymaps/iso_5x1u_split_rshift_neo2/config.h
@@ -0,0 +1,12 @@
+#pragma once
+#define UNICODE_SELECTED_MODES UC_WIN, UNICODE_MODE_WINCOMPOSE
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+// #include QMK_KEYBOARD_CONFIG_H
+
+
+#define TAPPING_TERM 175
+#define TAPPING_TOGGLE 2
+
+#endif

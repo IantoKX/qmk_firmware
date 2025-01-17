39dd941bdf1a64bf769c7d8ac39ef298c237a389
IantoKX <jan@stante-pe.de>
2025-01-17 19:32:25 +0100

    neo2 layout for dz60

--- keyboards/dz60/keymaps/iso_5x1u_split_rshift_neo2/rules.mk
+++ keyboards/dz60/keymaps/iso_5x1u_split_rshift_neo2/rules.mk
@@ -18,7 +18,10 @@ BOOTMAGIC_ENABLE = lite        # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes          # Mouse keys
 EXTRAKEY_ENABLE = yes          # Audio control and System control
 CONSOLE_ENABLE = no            # Console for debug
-COMMAND_ENABLE = no            # Commands for debug and configuration
+COMMAND_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no            # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no          # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
@@ -32,3 +35,5 @@ FAUXCLICKY_ENABLE = no         # Use buzzer to emulate clicky switches
 NO_USB_STARTUP_CHECK = no      # Disable initialization only when usb is plugged in
 UNICODE_ENABLE = yes					 # enable unicode input
 TAP_DANCE_ENABLE = yes			# enable Tap Dance
+LTO_ENABLE = yes
+MUSIC_ENABLE = no
